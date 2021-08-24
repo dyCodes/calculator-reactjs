e602044603790e8b1f8d50521ec2e9a94818658a
dyCodes <yusufdaudu51@gmail.com>
2021-08-24 03:23:14 +0100

    Updates

---
+++ precache-manifest.007961db9434ce61fdb42b72873938d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec89267c036e10449367e8a8557b8b99",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "b13d5290cbdd23ed0c56",
+    "url": "/calculator-reactjs/static/css/main.efb92317.chunk.css"
+  },
+  {
+    "revision": "55d9828ca1827bb8cae8",
+    "url": "/calculator-reactjs/static/js/2.a5d8ed2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-reactjs/static/js/2.a5d8ed2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13d5290cbdd23ed0c56",
+    "url": "/calculator-reactjs/static/js/main.7f823bb3.chunk.js"
+  },
+  {
+    "revision": "81e3c7c1dbb8a87ecf42",
+    "url": "/calculator-reactjs/static/js/runtime-main.510c1274.js"
+  }
+]);
